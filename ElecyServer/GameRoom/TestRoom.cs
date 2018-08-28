93374fc7651212d0084203efb29456476f386992
Ludaris <Luda@github.com>
2018-08-28 16:03:29 +0300

    [Final] TestRoom

---
+++ ElecyServer/GameRoom/TestRoom.cs
@@ -0,0 +1,20 @@
+﻿using Bindings;
+
+namespace ElecyServer
+{
+    public class TestRoom : BaseGameRoom
+    {
+
+        #region Constructor
+
+        public TestRoom(ClientTCP client, int mapIndex) : base(client, 1, mapIndex) { }
+
+        protected internal override void RoomType()
+        {
+            this.roomType = RoomTypes.TestRoom;
+        }
+
+        #endregion
+
+    }
+}
